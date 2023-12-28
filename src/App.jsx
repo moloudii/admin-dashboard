aef9d870a5d66ac54f638e641f10a2ef94975f79
moloudii <mhmmdalinaz@gmail.com>
2023-12-28 12:38:39 +0330

    Register template

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import Login from "./features/identity/components/Login";
+import Register from "./features/identity/components/Register";
 
 function App() {
-  return <Login />;
+  return <Register />;
 }
 
 export default App;
