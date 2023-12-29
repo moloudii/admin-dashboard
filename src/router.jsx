6b0654399bc880641d6b4126f192677d5b41f562
moloudii <mhmmdalinaz@gmail.com>
2023-12-29 13:54:15 +0330

    Define the identityLayout component

--- src/router.jsx
+++ src/router.jsx
@@ -1,12 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "./features/identity/components/Login";
 import Register from "./features/identity/components/Register";
+import IdentityLayout from "./layouts/identity-layout";
 
 const router = createBrowserRouter([
-  { path: "login", element: <Login /> },
   {
-    path: "register",
-    element: <Register />,
+    element: <IdentityLayout />,
+    children: [
+      { path: "login", element: <Login /> },
+      {
+        path: "register",
+        element: <Register />,
+      },
+    ],
   },
 ]);
 
