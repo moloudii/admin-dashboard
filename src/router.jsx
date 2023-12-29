5958778aee6a8ab94b48a9e27f199dc54c2d6ce4
moloudii <mhmmdalinaz@gmail.com>
2023-12-29 13:44:13 +0330

    install react-router-dom

---
+++ src/router.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+import Login from "./features/identity/components/Login";
+import Register from "./features/identity/components/Register";
+
+const router = createBrowserRouter([
+  { path: "login", element: <Login /> },
+  {
+    path: "register",
+    element: <Register />,
+  },
+]);
+
+export default router;
