4772e26d5fb4ba527e3028814ea2029f47036adf
moloudii <mhmmdalinaz@gmail.com>
2024-01-01 21:27:42 +0330

    Interceptor implementation

--- src/core/http-service.js
+++ src/core/http-service.js
@@ -6,3 +6,18 @@ export const httpService = axios.create({
   baseURL: BASE_URL,
 });
 
+const httpInterceptedService = axios.create({
+  baseURL: BASE_URL,
+});
+httpInterceptedService.interceptors.request.use(
+  async (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers = {
+        authorization: `Bearer ${token}`,
+      };
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
