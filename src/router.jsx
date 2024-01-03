c67ccc2c01fb8d050329d905c2198bd3981ff81c
moloudii <mhmmdalinaz@gmail.com>
2024-01-03 21:56:33 +0330

    dynamic route

--- src/router.jsx
+++ src/router.jsx
@@ -7,6 +7,9 @@ import IdentityLayout from "./layouts/identity-layout";
 import MainLayout from "./layouts/mainLayout/main-layout";
 import Courses, { coursesLoader } from "./pages/courses";
 import CourseCategories from "./pages/course-categories";
+import CourseDetails, {
+  courseDetailsLoader,
+} from "./features/courses/components/course-details";
 
 const router = createBrowserRouter([
   {
@@ -21,6 +24,11 @@ const router = createBrowserRouter([
       {
         path: "course-categories",
         element: <CourseCategories />,
+      },
+      {
+        path: "courses/:id",
+        element: <CourseDetails />,
+        loader: courseDetailsLoader,
       },
     ],
   },
