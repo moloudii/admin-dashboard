52657d2a13dbf237c54c57caa42f207068ab6ab1
moloudii <mhmmdalinaz@gmail.com>
2024-01-04 17:53:22 +0330

    fetch and render Category list

--- src/router.jsx
+++ src/router.jsx
@@ -6,7 +6,7 @@ import Register, {
 import IdentityLayout from "./layouts/identity-layout";
 import MainLayout from "./layouts/mainLayout/main-layout";
 import Courses, { coursesLoader } from "./pages/courses";
-import CourseCategories from "./pages/course-categories";
+import CourseCategories, { categoriesLoader } from "./pages/course-categories";
 import CourseDetails, {
   courseDetailsLoader,
 } from "./features/courses/components/course-details";
@@ -24,6 +24,7 @@ const router = createBrowserRouter([
       {
         path: "course-categories",
         element: <CourseCategories />,
+        loader: categoriesLoader,
       },
       {
         path: "courses/:id",
