d54fcfe8bfe09c823157974680b0e160ee698347
moloudii <mhmmdalinaz@gmail.com>
2024-01-06 10:36:47 +0330

    Modal template with the help of Portal

--- src/features/categories/components/category-list.jsx
+++ src/features/categories/components/category-list.jsx
@@ -2,7 +2,10 @@ import { useNavigation } from "react-router-dom";
 import Pagination from "../../../components/pagination";
 import Spinner from "../../../components/spinner";
 
-function CategoryList({ categories: { data, totalRecords } }) {
+function CategoryList({
+  categories: { data, totalRecords },
+  setShowDeleteModal,
+}) {
   const navigation = useNavigation();
   return (
     <>
@@ -36,7 +39,7 @@ function CategoryList({ categories: { data, totalRecords } }) {
                           <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path>
                         </svg>
                       </a>
-                      <a>
+                      <a onClick={() => setShowDeleteModal(true)}>
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
                           width="24"
