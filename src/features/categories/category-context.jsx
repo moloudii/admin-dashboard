8381712f63663918cfff3bbcf15ba775c9c316b4
moloudii <mhmmdalinaz@gmail.com>
2024-01-06 20:20:15 +0330

    define category context

---
+++ src/features/categories/category-context.jsx
@@ -0,0 +1,17 @@
+import { createContext, useContext, useState } from "react";
+
+const CategoryContext = createContext();
+
+const CategoryProvider = ({ children }) => {
+  const [category, setCategory] = useState();
+
+  return (
+    <CategoryContext.Provider value={{ category, setCategory }}>
+      {children}
+    </CategoryContext.Provider>
+  );
+};
+const useCategoryContext = () => {
+  return useContext(CategoryContext);
+};
+export { useCategoryContext, CategoryProvider };
